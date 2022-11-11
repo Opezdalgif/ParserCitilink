e324a5e715baeb924fe8f56f045d03c06f541ecd
Opezdalgif <71887068+Opezdalgif@users.noreply.github.com>
2022-11-11 14:37:24 +0500

    Добавление dnsParser

---
+++ dnsParser.js
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer');
+
+DnsScrapper();
+async function DnsScrapper() {
+
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 30, // замедляет работу браузера
+        ignoreHTTPSErrors: true,
+        args: [
+          `--window-size=1920,1080`
+        ],
+        defaultViewport: {
+          width: 1920,
+          height: 1080
+        }
+      });
+    
+        const articul = `9S7-14D334-1226`
+        const dnsPage = await browser.newPage()
+        await dnsPage.goto(`https://www.dns-shop.ru/search/?q=${articul}`)
+        let price = await dnsPage.evaluate(() => {
+            return Array.from(document.querySelectorAll('.product-buy__price').values()).map(el => el.textContent)
+        })
+        console.log(price[0])
+        await dnsPage.click('.product-characteristics__footer > button')
+        let gabarit = await dnsPage.evaluate(() => {
+            return Array.from(document.querySelectorAll('.product-characteristics__spec ')).map(el => el.textContent)  
+            //Array.from(document.querySelectorAll('.product-characteristics__spec-value').values()).map(el => el.textContent)
+            
+        })
+        console.log(gabarit)
+    
+}
